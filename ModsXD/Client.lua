fedeb06459162a73f3d995c1c9d8aa213399b08c
Mohammad-PCSX <100066111+Mohammad-PCSX@users.noreply.github.com>
2022-07-12 10:38:30 +0430

    Add files via upload

---
+++ ModsXD/Client.lua
@@ -0,0 +1,7 @@
+engineImportTXD(engineLoadTXD("411.txd"),360)
+engineReplaceModel(engineLoadDFF("411.dff"),360)
+
+engineImportTXD(engineLoadTXD("300.txd"),300)
+engineReplaceModel(engineLoadDFF("300.dff"),300)
+
+createVehicle(411,-1255.48291, 499.60025, 18.23438)
